f3d3de0f0f6bc66084f8f7f96fe4d19b1e3bf330
JavierOramas <javiale2000@gmail.com>
2022-11-13 21:09:48 -0500

    interactive querys

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ from boolean_model import BooleanModel
 if __name__ == "__main__":
     model = BooleanModel('./corpus/*', 'english')
     # print(model.vocabulary)
-    print(model.query("recipe"))
+    while(True):
+        txt = input(">>> ")
+        print(model.query(txt))
