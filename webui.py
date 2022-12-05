faec1f8da2bc113be14568c15c47d5ebcb3d2e70
JavierOramas <javiale2000@gmail.com>
2022-12-05 13:32:54 -0500

    webui

--- webui.py
+++ webui.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect
 from boolean_model import BooleanModel
 
 # Flask app boilerplate
 app = Flask(__name__)
-model = BooleanModel('./corpus/soccer/*', 'english')
+boolean_model = BooleanModel('./corpus/soccer/*', 'english')
 
 @app.route("/", methods = ["GET"])
 def home():
-    return 
+    return redirect("/search")
+@app.route('/search', methods = ["GET", "POST"])
+def search():
+    res = []
+    
+    if request.method == "POST":
+        query = request.form['search']
+        # query_method = request.form['query_method']
+        
+        # if query_method == "boolean":
+        res = boolean_model.query(query)
+        print(res)
+        return render_template('search.html', results=res)
+    return render_template('search.html')
+     
+if __name__ == "__main__":
+    app.run(debug=True)
