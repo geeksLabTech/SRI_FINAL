71d82077b2038e40b34f5c0f71f9176537630146
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2022-12-05 00:23:25 -0500

    Impl class for handling all models

---
+++ document_data.py
@@ -0,0 +1,6 @@
+
+class DocumentData:
+    def __init__(self, path: str, lenght: int, max_frequency_term: int) -> None:
+        self.path = path 
+        self.length = lenght
+        self.max_frequency_term = max_frequency_term
