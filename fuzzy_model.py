d46aeaaae71d23787f3f2e87a567fd60e872105d
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2022-12-21 02:03:49 -0500

    Clean fuzzy_model

--- fuzzy_model.py
+++ fuzzy_model.py
@@ -15,18 +15,10 @@ class FuzzyModel(BooleanModel):
         self.documents = documents
         self.fuzzy = True
         self.vocabulary_dict = vocabulary_dict
-        self.all_words_nodes: list[TrieNode] = []
     
     def query(self, tokenized_query: list) -> list[tuple[int, float]]:
         processed_query = self.proccess_query(tokenized_query)   
         relevant_documents = self.eval_query(tokenized_query, processed_query)
-
-        # result = []
-        # for doc in relevant_documents:
-        #     if relevant_documents[doc] >= 0.1:
-        #         result.append((doc, relevant_documents[doc]))
-        # print(result)
-        # sort result by relevance
         return sorted(relevant_documents.items(), key=lambda x: x[1], reverse=True)
 
     def eval_query(self,tokenized_query, processed_query):
@@ -41,21 +33,16 @@ class FuzzyModel(BooleanModel):
             cdnf_query = [x for x in processed_query if not x==')' and not x=='(' and not x=='&' and not x=='|']
         
         print(cdnf_query)
-        # self.search_all_words_nodes(self.trie.root)
         dic_queryterm_with_doc = self.calculate_rank(tokenized_query, cdnf_query)
         dic_recall = self.recall(dic_queryterm_with_doc)
-        # print(dic_recall , 'rank')
-        # sorted_documents = sorted(dic_recall.items(), key=lambda x: x[1], reverse=True)
         return dic_recall
 
     def calculate_rank(self, tokenized_query, cdnf_query):
         fuzzy_set_of_term = {}
         rank_of_document = {}
         for token in tokenized_query:
-            # node = self.trie.search(token)
             fuzzy_set_of_term[token] = self.calculate_fuzzy_set(token)
             
-        
         for term in cdnf_query:
             is_negated = False
             if term[0] == '~':
@@ -73,24 +60,6 @@ class FuzzyModel(BooleanModel):
                         rank_of_document[doc] *= -fuzzy_set_of_term[term][doc]
                     else :
                         rank_of_document[doc] *= 1 - fuzzy_set_of_term[term][doc]
-            # for word in words:
-            #     try:
-            #         correlation_of_terms = correlation_terms_with_words[term][word]
-            #     except KeyError:
-            #         continue
-            #     if correlation_of_terms == None:
-            #         continue
-                # if is_negated:
-                #     correlation_of_terms = 1 - correlation_of_terms
-                #     if math.isclose(correlation_of_terms, 0.0):
-                #         continue
-
-                # for doc_id in word.frequency_by_document.keys():
-
-                #     if doc_id not in correlation_term_of_query_with_doc:
-                #         correlation_term_of_query_with_doc[doc_id] = correlation_of_terms
-                #     else:
-                #         correlation_term_of_query_with_doc[doc_id] *= (1 - correlation_of_terms)
                         
         return rank_of_document
 
@@ -101,12 +70,6 @@ class FuzzyModel(BooleanModel):
             for doc in document_of_term:
                 dict_corr_between_term_doc[doc] = get_tf(document_of_term[doc],self.documents[doc].length)*get_idf(len(self.documents),len(document_of_term)) 
         return dict_corr_between_term_doc
-            
-    # def search_all_words_nodes(self, root: TrieNode):
-    #     if root.is_word():
-    #         self.all_words_nodes.append(root)
-    #     for next_node in root.transitions.values():
-    #         self.search_all_words_nodes(next_node)
         
     def recall(self,dic_correlation):
         new_dic_correlation = {}
