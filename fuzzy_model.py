f40bcd2d82c3dabd54908bab7903b0496b5e7679
Lia <liazerqueraf@gmail.com>
2022-12-20 23:27:50 -0500

    rank in the fuzzy model

--- fuzzy_model.py
+++ fuzzy_model.py
@@ -6,20 +6,19 @@ from sympy import to_dnf, sympify
 from trie import Trie , TrieNode
 from query_tools import get_type_of_token, infix_to_postfix
 from nltk.tokenize import word_tokenize
+from vectorization_utils import get_idf , get_tf
+from document_data import DocumentData
 
 class FuzzyModel(BooleanModel):
-    def __init__(self,trie: Trie , documents):
+    def __init__(self,vocabulary_dict: dict[str,dict[int,int]] ,trie : Trie, documents:dict[int, DocumentData]):
         super().__init__(trie,documents)
+        self.documents = documents
         self.fuzzy = True
-        self.trie = trie
+        self.vocabulary_dict = vocabulary_dict
         self.all_words_nodes: list[TrieNode] = []
     
     def query(self, tokenized_query, target_relevance):
         processed_query = self.proccess_query(tokenized_query)   
-        # query_done = ''
-        # for term in processed_query:
-        #     query_done +=term
-
         relevant_documents = self.eval_query(tokenized_query, processed_query)
 
         result = []
@@ -42,18 +41,18 @@ class FuzzyModel(BooleanModel):
             cdnf_query = [x for x in processed_query if not x==')' and not x=='(' and not x=='&' and not x=='|']
         
         print(cdnf_query)
-        self.search_all_words_nodes(self.trie.root)
-        dic_queryterm_with_doc = self.build_correlation_matrix(tokenized_query, cdnf_query,self.all_words_nodes)
+        # self.search_all_words_nodes(self.trie.root)
+        dic_queryterm_with_doc = self.calculate_rank(tokenized_query, cdnf_query)
         dic_recall = self.recall(dic_queryterm_with_doc)
         return dic_recall
 
-    def build_correlation_matrix(self, tokenized_query, cdnf_query, words: list[TrieNode]):
-        correlation_term_of_query_with_doc = {}
-        correlation_terms_with_words = {}
+    def calculate_rank(self, tokenized_query, cdnf_query):
+        fuzzy_set_of_term = {}
+        rank_of_document = {}
         for token in tokenized_query:
-            node = self.trie.search(token)
-            if not node == None:
-                correlation_terms_with_words[token] = self.calculate_correlation_between_term_and_words(node, words)
+            # node = self.trie.search(token)
+            fuzzy_set_of_term[token] = self.calculate_fuzzy_set(token)
+            
         
         for term in cdnf_query:
             is_negated = False
@@ -61,46 +60,51 @@ class FuzzyModel(BooleanModel):
                 term = term[1:]
                 is_negated = True
             
-            for word in words:
-                try:
-                    correlation_of_terms = correlation_terms_with_words[term][word]
-                except KeyError:
-                    continue
-                if correlation_of_terms == None:
-                    continue
-                if is_negated:
-                    correlation_of_terms = 1 - correlation_of_terms
-                    if math.isclose(correlation_of_terms, 0.0):
-                        continue
+            for doc in fuzzy_set_of_term[term]:
+                if not doc in rank_of_document:
+                    if is_negated:
+                        rank_of_document[doc] = -fuzzy_set_of_term[term][doc]
+                    else :
+                        rank_of_document[doc] = 1 - fuzzy_set_of_term[term][doc]
+                else:
+                    if is_negated:
+                        rank_of_document[doc] *= -fuzzy_set_of_term[term][doc]
+                    else :
+                        rank_of_document[doc] *= 1 - fuzzy_set_of_term[term][doc]
+            # for word in words:
+            #     try:
+            #         correlation_of_terms = correlation_terms_with_words[term][word]
+            #     except KeyError:
+            #         continue
+            #     if correlation_of_terms == None:
+            #         continue
+                # if is_negated:
+                #     correlation_of_terms = 1 - correlation_of_terms
+                #     if math.isclose(correlation_of_terms, 0.0):
+                #         continue
 
-                for doc_id in word.frequency_by_document.keys():
+                # for doc_id in word.frequency_by_document.keys():
 
-                    if doc_id not in correlation_term_of_query_with_doc:
-                        correlation_term_of_query_with_doc[doc_id] = correlation_of_terms
-                    else:
-                        correlation_term_of_query_with_doc[doc_id] *= (1 - correlation_of_terms)
+                #     if doc_id not in correlation_term_of_query_with_doc:
+                #         correlation_term_of_query_with_doc[doc_id] = correlation_of_terms
+                #     else:
+                #         correlation_term_of_query_with_doc[doc_id] *= (1 - correlation_of_terms)
                         
-        return correlation_term_of_query_with_doc
+        return rank_of_document
 
-    def calculate_correlation_between_term_and_words(self, node_term: TrieNode, words: list[TrieNode]):
-        correlation_term_with_words = {}
-        for word in words:
-            number_document_included_this_term = len(word.frequency_by_document)
-            document_included_term_of_query = len(node_term.frequency_by_document)
-            number_of_common_document = len(set(word.frequency_by_document.keys()).intersection(set(node_term.frequency_by_document.keys())))
-            corr =  number_of_common_document/(document_included_term_of_query + number_document_included_this_term - number_of_common_document)
-            if math.isclose(corr, 0.0):
-                correlation_term_with_words[word] = None
-            else:
-                correlation_term_with_words[word] = 1 - corr
-        
-        return self.recall(correlation_term_with_words)
-
-    def search_all_words_nodes(self, root: TrieNode):
-        if root.is_word():
-            self.all_words_nodes.append(root)
-        for next_node in root.transitions.values():
-            self.search_all_words_nodes(next_node)
+    def calculate_fuzzy_set(self, term):
+        dict_corr_between_term_doc = {}
+        if term in self.vocabulary_dict:
+            document_of_term = self.vocabulary_dict[term]
+            for doc in document_of_term:
+                dict_corr_between_term_doc[doc] = get_tf(document_of_term[doc],self.documents[doc].length)*get_idf(len(self.documents),len(document_of_term)) 
+        return dict_corr_between_term_doc
+            
+    # def search_all_words_nodes(self, root: TrieNode):
+    #     if root.is_word():
+    #         self.all_words_nodes.append(root)
+    #     for next_node in root.transitions.values():
+    #         self.search_all_words_nodes(next_node)
         
     def recall(self,dic_correlation):
         new_dic_correlation = {}
